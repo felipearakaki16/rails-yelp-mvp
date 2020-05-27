03a2028a3b258d6f7f770536530b8eb2d3606b77
Felipe Arakaki <felipearakaki81@yahoo.com.br>
2020-05-27 17:29:49 -0300

    Restaurants working, missing review.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :restaurants, only: %i[ index create new show edit ] do
+    resources :reviews, only: :destroy
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create([
+                                  { 
+                                    name: 'Restaurante Um',
+                                    address: 'Rua Tal, 123',
+                                    phone_number: '985 745 845',
+                                    category: 'chinese'
+                                  },
+                                  { 
+                                    name: 'Restaurante Dois',
+                                    address: 'Rua alalalong, 123',
+                                    phone_number: '574 894 124',
+                                    category: 'japanese'
+                                  },
+                                  { 
+                                    name: 'Restaurante Três',
+                                    address: 'Rua fjvndfnvdf, 3',
+                                    phone_number: '985514 478 963',
+                                    category: 'chinese'
+                                  },
+                                  { 
+                                    name: 'Restaurante Quatro',
+                                    address: 'Rua Taldwqdwqdq, 1123',
+                                    phone_number: '748 123 584',
+                                    category: 'italian'
+                                  },
+                                  { 
+                                    name: 'Restaurante Cinco',
+                                    address: 'Rua cdsnvskvjc, 123',
+                                    phone_number: '857 625 145',
+                                    category: 'belgian'
+                                  }
+                                ])
